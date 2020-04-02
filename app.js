28ca3c1d1be3b332e3bd3f8a1aa917ce88ebf334
rafapignataro <rafapignataro@gmail.com>
2020-04-01 22:19:12 -0300

    new home/edits

--- app.js
+++ app.js
@@ -44,5 +44,5 @@ io.on('connection', socket => {
 });
 
 server.listen(process.env.PORT || 3000, process.env.IP, function () {
-    console.log('servern on...')
+    console.log('server on...')
 });
